35c78b28dd91c38639ba416028750d79e00060ea
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 21:42:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,5 @@
 def oxford_comma(array)
   new_array = array[0..array.size - 2]
   new_array << "and #{array[array.size - 1]}"
-  string = array.join(", ")
-  string[array.si]
+  new_array.join(", ")
 end
