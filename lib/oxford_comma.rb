7e6cd14aba508a69edcbf807cac42472252c18f4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 21:47:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      return array[0]
+    when 2
+      return "#{array[0]} and #{array[1]}"
+    else
+      new_array = array[0..array.size - 2]
+      new_array << "and #{array[array.size - 1]}"
+      new_array.join(", ")
+    end
 end
